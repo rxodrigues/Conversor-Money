568f24c02d57c5e49d8b0caf7f63d2972d9d0ba9
Leonardo <rxodrigues.com>
2024-03-06 14:14:08 -0300

    conversor money

---
+++ script.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit)
+
+const inputValue = document.getElementById('value-real');
+const selectedCurrency = document.getElementById('currency');
+const result = document.getElementById('result');
+
+function handleSubmit(e) {
+    e.preventDefault();
+   
+    if(!inputValue.value || inputValue.value < 0){
+        alert('Informe um valor correto!')
+        return;
+    } else if(!selectedCurrency.value) {
+        alert('Escolha uma moeda');
+        return;
+    }
+
+  
+    converter();
+};
+
+function converter() {
+    if(selectedCurrency.value === 'eur') {
+        valueConverted = inputValue.value / 5.36;
+        result.innerHTML = valueFormatter ('pt-BR', 'EUR');
+
+        animateResult();
+   
+    } else if (selectedCurrency.value === 'dol') {
+        valueConverted = inputValue.value / 4.93;
+        result.innerHTML = valueFormatter('en-US', 'USD');
+
+        animateResult();
+    }
+
+    inputValue.value = '';
+    selectedCurrency.value ='';    
+};
+
+function valueFormatter(Locale, currency ) {
+    const value = valueConverted.toLocaleString(`${Locale}`, { style: 'currency', currency: `${currency}` })
+    return `<span>💵</span> ${value} <span>💵</span>`;
+};
+
+function animateResult() {
+    return result.animate([
+        { transform: 'translateY(-150px'},
+        { transform: 'translateY(0px)'},       
+    ], { duration: 500 });
+}
